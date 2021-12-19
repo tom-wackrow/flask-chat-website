4dc115486055aa8cb75972fc077446af85cda343
tommytank20 <wackrowtom@gmail.com>
2021-12-19 17:18:26 +0000

    fixed __init__.py

--- start_server.bat
+++ start_server.bat
@@ -1,4 +1,5 @@
 @REM venv\Scripts\python.exe -m pip install flask flask-login flask-wtf flask-sqlalchemy flask-migrate
 cls
-start http://127.0.0.1:5000
+@REM start http://127.0.0.1:5000
 venv\Scripts\flask.exe run
+pause
