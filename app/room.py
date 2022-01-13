a34ff57cbaa0ccf16e798e789c9264b4b24a45a3
tommytank20 <wackrowtom@gmail.com>
2022-01-13 15:09:25 +0000

    revert

--- app/room.py
+++ app/room.py
@@ -1,10 +1,13 @@
+from werkzeug.utils import redirect
 from app import socketio
 from flask_socketio import join_room, leave_room
 from flask_login import current_user
-
+from flask import redirect
 
 @socketio.on('join', namespace='/room')
 def join(message):
+    if current_user.room == None:
+        return redirect("index.html")
     room = current_user.room
     join_room(room, namespace="/room")
     socketio.emit("message", message, room=room, namespace="/room")
@@ -12,11 +15,15 @@ def join(message):
 
 @socketio.on('message', namespace='/room')
 def handle_message(message):
+    if message["message"] == "":
+        pass
     room = current_user.room
     socketio.emit("message", message, room=room, namespace="/room")
 
 @socketio.on("disconnect", namespace="/room")
-def disconnect(message):
+def disconnect():
+    if current_user.room == None:
+        return redirect("index.html")
     room = current_user.room
     leave_room(room)
     current_user.room = None
